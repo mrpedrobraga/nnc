ece23ed38912f643e39886019cb0492dc66a2b53
Pedro Braga <mrhenribraga@gmail.com>
2023-09-02 22:20:49 -0300

    Added a basic CLI frontend.

--- src/file_importer.rs
+++ src/file_importer.rs
@@ -1,3 +1,13 @@
-pub fn hi() {
-    println!("Hi!")
+use std::fs;
+
+pub fn import_nano_source(path: &str) -> String {
+    let file_read_result = fs::read_to_string(path);
+
+    match file_read_result {
+        Ok(content) => {
+            println!("{}", content);
+            return content;
+        },
+        Err(error) => panic!("File '{}' couldn't be loaded {:?}.", path, error)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use std::env;
+
 mod file_importer;
 
 fn main() {
-    file_importer::hi()
+    let args: Vec<String> = env::args().collect();
+    let version = "0.0.1";
+
+    if args.len() < 2 {
+        println!("`nnc` - the `nano` compiler\nVersion: '{}'.\n\nUse `nnc` help to see a quick manual.", version);
+        return
+    }
+    
+    match args[1].as_str() {
+        "help" => print_help(),
+        "compile" => {}
+        _ => {
+            println!("Subcommand '{}' not recognized.\nUse `nnc help` to see a quick manual.", args[1])
+        }
+    }
+
+    //file_importer::import_nano_source("./examples/hello_world.nano");
+}
+
+fn print_help() {
+    println!("Usage: `nnc <subcommand>`.");
+    println!("");
+    println!("compile <entry_file>\n - Begins compilation starting at <entry_file>.\n");
+    println!("run <entry_file>\n - Same as `compile`, but immediately runs the exported executable.\n");
+    println!("clean\n - Cleans unnecessary cache.\n");
+    println!("test <entry_file>\n - Begins testing starting at <entry_file>.\n");
+    println!("lint <file>\n - Locates linting configuration in the workspace, then provides ERRORs and WARNINGs for a file.\n");
+    println!("fmt <file>\n - Locates a style configuration and formats the file accordingly.\n");
+    println!("lsp <port>\n - Starts the language server in a given port.\n");
+    println!("The compilation parameters and flags can be read from your nano source to further alter compilation/testing params.\n")
 }
