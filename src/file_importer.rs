57c323f28a9dc2f307ae621c4c39fc175fbe1b6f
Pedro Braga <mrhenribraga@gmail.com>
2023-09-02 22:53:59 -0300

    File importer can now read the contents of a file.

--- src/file_importer.rs
+++ src/file_importer.rs
@@ -2,7 +2,6 @@ use std::fs;
 
 pub fn import_nano_source(path: &str) -> Result<String, std::io::Error> {
     let file_read_result = fs::read_to_string(path);
-    let _ = fs::create_dir(path);
 
     return file_read_result;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error};
+use std::env;
 
 use file_importer::import_nano_source;
 
